19ea659715c4fde11aeb9538942952c671aaa2c2
Black <guobin_xu@yeah.net>
2016-07-17 22:50:33 +0800

    update masterpage nav event

--- HZCGZM_Web/Front/base.Master.cs
+++ HZCGZM_Web/Front/base.Master.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,10 +10,103 @@ namespace HZCGZM_Web.Web
 {
     public partial class web : System.Web.UI.MasterPage
     {
+        const int NAV_MAIN = 1;
+        const int NAV_ABOUT = 2;
+        const int NAV_NEWS = 3;
+        const int NAV_PRODUCT = 4;
+        const int NAV_APPLICATION = 5;
+        const int NAV_SUPPORT = 6;
+        const int NAV_DOWNLOAD = 7;
+        const int NAV_CONTACT = 8;
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            //默认选中首页
+            updateNav(NAV_MAIN);
         }
+
+        protected void lkbMain_Click(object sender, EventArgs e)
+        {
+            updateNav(NAV_MAIN);
+        }
+
+        protected void lkbAbout_Click(object sender, EventArgs e)
+        {
+            updateNav(NAV_ABOUT);
+        }
+
+        protected void lkbNews_Click(object sender, EventArgs e)
+        {
+            updateNav(NAV_NEWS);
+        }
+
+        protected void lkbProduct_Click(object sender, EventArgs e)
+        {
+            updateNav(NAV_PRODUCT);
+        }
+
+        protected void lkbApplication_Click(object sender, EventArgs e)
+        {
+            updateNav(NAV_APPLICATION);
+        }
+
+        protected void lkbSupport_Click(object sender, EventArgs e)
+        {
+            updateNav(NAV_SUPPORT);
+        }
+
+        protected void lkbDownload_Click(object sender, EventArgs e)
+        {
+            updateNav(NAV_DOWNLOAD);
+        }
+
+        protected void lkbContact_Click(object sender, EventArgs e)
+        {
+            updateNav(NAV_CONTACT);
+        }
+
+        private void updateNav(int navType)
+        {
+            lkbMain.ForeColor = Color.FromArgb(51, 51, 51);
+            lkbAbout.ForeColor = Color.FromArgb(51, 51, 51);
+            lkbNews.ForeColor = Color.FromArgb(51, 51, 51);
+            lkbProduct.ForeColor = Color.FromArgb(51, 51, 51);
+            lkbApplication.ForeColor = Color.FromArgb(51, 51, 51);
+            lkbSupport.ForeColor = Color.FromArgb(51, 51, 51);
+            lkbDownload.ForeColor = Color.FromArgb(51, 51, 51);
+            lkbContact.ForeColor = Color.FromArgb(51, 51, 51);
+
+            switch (navType)
+            {
+                case NAV_MAIN:
+                    lkbMain.ForeColor = Color.FromArgb(255, 127, 0);
+                    break;
+                case NAV_ABOUT:
+                    lkbAbout.ForeColor = Color.FromArgb(255, 127, 0);
+                    break;
+                case NAV_NEWS:
+                    lkbNews.ForeColor = Color.FromArgb(255, 127, 0);
+                    break;
+                case NAV_PRODUCT:
+                    lkbProduct.ForeColor = Color.FromArgb(255, 127, 0);
+                    break;
+                case NAV_APPLICATION:
+                    lkbApplication.ForeColor = Color.FromArgb(255, 127, 0);
+                    break;
+                case NAV_SUPPORT:
+                    lkbSupport.ForeColor = Color.FromArgb(255, 127, 0);
+                    break;
+                case NAV_DOWNLOAD:
+                    lkbDownload.ForeColor = Color.FromArgb(255, 127, 0);
+                    break;
+                case NAV_CONTACT:
+                    lkbContact.ForeColor = Color.FromArgb(255, 127, 0);
+                    break;
+            }
+         
+        }
+
+        
 
     }
 }
--- HZCGZM_Web/Front/base.Master.designer.cs
+++ HZCGZM_Web/Front/base.Master.designer.cs
@@ -85,13 +85,13 @@ namespace HZCGZM_Web.Web {
         protected global::System.Web.UI.WebControls.LinkButton lkbAbout;
         
         /// <summary>
-        /// hlkNews 控件。
+        /// lkbNews 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink hlkNews;
+        protected global::System.Web.UI.WebControls.LinkButton lkbNews;
         
         /// <summary>
         /// lkbProduct 控件。
