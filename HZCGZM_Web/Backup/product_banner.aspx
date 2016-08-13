889cd1aafdc32e9803d589b3b312972aa3cf3615
Black <guobin_xu@yeah.net>
2016-08-13 23:19:38 +0800

    product banner

---
+++ HZCGZM_Web/Backup/product_banner.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Backup/base.Master" AutoEventWireup="true" CodeBehind="product_banner.aspx.cs" Inherits="HZCGZM_Web.Backup.product_banner" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:SqlDataSource ID="sdcProductBanner" runat="server" ConnectionString="<%$ ConnectionStrings:HZCGZMConnectionString %>" SelectCommand="SELECT [imageId], [imageURL] FROM [tbImage] WHERE (([bindId] = @bindId) AND ([imageState] = @imageState) AND ([imageType] = @imageType))">
+        <SelectParameters>
+            <asp:QueryStringParameter DefaultValue="0" Name="bindId" QueryStringField="id" Type="Int32" />
+            <asp:Parameter DefaultValue="1" Name="imageState" Type="Int32" />
+            <asp:Parameter DefaultValue="7" Name="imageType" Type="Int32" />
+        </SelectParameters>
+    </asp:SqlDataSource>
+    <br />
+    <div>编辑产品展示图&nbsp;&nbsp;&nbsp;&nbsp;<a href='product_detail.aspx?id=<%=Request.QueryString["id"] %>'>返回详情</a></div>
+    <br />
+    <table class="table table-bordered table-hover table-striped">
+        <tbody>
+            <tr>
+                <td>产品图：</td>
+                <td>
+                    <asp:FileUpload ID="fuProductBanner" runat="server" />
+                    <asp:Button ID="btnAdd" runat="server" Text="添加" OnClick="btnAdd_Click" />
+                    注：建议尺寸 宽580px 高387px;
+                    <br />
+                    <br />
+                    <br />
+                    <ul>
+                        <asp:Repeater ID="rptProductBanner" runat="server" DataSourceID="sdcProductBanner">
+
+                            <ItemTemplate>
+                                <li>
+                                    <asp:Image ID="imgProductBanner" Width="580" Height="387" runat="server" ImageUrl='<%#Eval("imageURL") %>' />
+                                    <asp:LinkButton ID="lbDelete" runat="server" OnClick="lbDelete_Click">删除</asp:LinkButton>
+                                    <asp:HiddenField ID="hfImageId" runat="server" Value='<%#Eval("imageId") %>' />
+                                    <br />
+                                </li>
+                            </ItemTemplate>
+
+                        </asp:Repeater>
+                    </ul>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</asp:Content>
---
+++ HZCGZM_Web/Backup/product_banner.aspx.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using HZCGZM_Model;
+
+namespace HZCGZM_Web.Backup
+{
+    public partial class product_banner : System.Web.UI.Page
+    {
+
+        private int id;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            id = Convert.ToInt32(Request.QueryString["id"]);
+            if (id <= 0)
+            {
+                Response.Write("<script>alert('入参错误')</script>");
+
+                return;
+            }
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (fuProductBanner.HasFile)
+            {
+                string fileExrensio = System.IO.Path.GetExtension(fuProductBanner.FileName).ToLower();
+                string fileType = fuProductBanner.PostedFile.ContentType;
+                string fileName = DateTime.Now.ToString("yyyyMMddHHmmssffff") + fileExrensio;
+                string uploadURL = Server.MapPath("~/upload/") + fileName;
+                string imageURL = "../upload/" + fileName;
+                if (fileType == "image/bmp" || fileType == "image/gif" || fileType == "image/jpeg" || fileType == "image/jpg" || fileType == "image/png")
+                {
+
+                    try
+                    {
+                        fuProductBanner.PostedFile.SaveAs(uploadURL);
+
+                        HZCGZMEntities entity = new HZCGZMEntities();
+                        tbImage image = new tbImage();
+                        image.bindId = id;
+                        image.imageState = (int)NormalState.AVALIABLE;
+                        image.imageType = (int)ImageType.PRODUCT_BANNER;
+                        image.imageURL = imageURL;
+
+                        entity.tbImage.Add(image);
+
+                        if (entity.SaveChanges() > 0)
+                        {
+                            Response.Write("<script>alert('上传成功')</script>");
+                            rptProductBanner.DataBind();
+                        }
+                        else
+                        {
+                              Response.Write("<script>alert('上传失败')</script>");
+                              return;
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Response.Write("<script>alert('上传失败')</script>");
+
+                        return;
+                    }
+                }
+                else
+                {
+                    Response.Write("<script>alert('格式错误')</script>");
+
+                    return;
+                }
+            }
+            else
+            {
+                Response.Write("<script>alert('请选择图片')</script>");
+                return;
+            }
+
+
+        }
+
+        protected void lbDelete_Click(object sender, EventArgs e)
+        {
+            RepeaterItem i = ((LinkButton)sender).Parent as RepeaterItem;
+            int imageId = Convert.ToInt32(((HiddenField)i.FindControl("hfImageId")).Value);
+
+            HZCGZMEntities entity = new HZCGZMEntities();
+            entity.tbImage.Where(m => m.imageId == imageId).FirstOrDefault().imageState = (int)NormalState.UNAVALIABLE;
+            if (entity.SaveChanges() > 0)
+            {
+                Response.Write("<script>alert('删除成功')</script>");
+                rptProductBanner.DataBind();
+            }
+            else
+            {
+                Response.Write("<script>alert('删除失败')</script>");
+            }
+        }
+    }
+}
